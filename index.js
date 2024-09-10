b05af6cb8892ff14d103f381741830d350b0b286
Lukitas <lvd.follert@gmail.com>
2024-09-10 15:38:13 +0200

    updated search on index page

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// get header form and input html element and store both into variables
-const searchForm = document.getElementById("header-form");
-const searchInput = document.getElementById("header-form-input");
-//we need to submit a new URL when search is used
-const searchURL = 'https://api.themoviedb.org/3'+'/search/movie?'+`${API_KEY}`+'&query='
-
 //TMDB, URL requests
 
 const API_KEY = 'api_key=e13239c36d67aaae5fd854a738bd27ed';
@@ -43,12 +37,20 @@ function showMovies(data){
     })
 }
 
+
+// get header form and input html element and store both into variables
+const searchForm = document.getElementById("header-form");
+const searchInput = document.getElementById("header-form-input");
+//we need to submit a new URL when search is used
+const searchURL = 'https://api.themoviedb.org/3'+'/search/movie?'+`${API_KEY}`+'&query='
+
+
 // because we used a form both enter and button click should submit the search
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
 // the string submitted in the form is stored in the variable searchTerm so we can append it to the regular search
-    const searchTerm = searchForm.value;
+    const searchTerm = searchInput.value;
 // if search term exists we add it to search url and use it, otherwise we perform regular search
     if (searchTerm) {
         getMovies(searchURL+searchTerm)
